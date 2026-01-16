d1119a766907e2ab6ce7fb83e93e75cc463dd65f
AbhIfine <abhi926987@gmail.com>
2026-01-16 11:25:15 +0530

    add extra keys and values

--- numpy_vs_loops_speed_test.py
+++ numpy_vs_loops_speed_test.py
@@ -12,7 +12,7 @@ data = {
     'name': np.random.choice(name_list, size=n),
     'Age': np.random.randint(18,90, size=n),
     'City': np.random.choice(city_list, size=n),
-    'Category' : np.random.choice(Category_list, size=n)
-
-
+    'Category' : np.random.choice(Category_list, size=n),
+    'Amount': np.random.randint(100,1000, size=n),
+    'Rating': (np.random.uniform(1,5, size=n),1)
 }
