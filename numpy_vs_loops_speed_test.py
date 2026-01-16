4786b0d6a37b0b22e0ab7651d9984980dbf65c43
AbhIfine <abhi926987@gmail.com>
2026-01-16 19:19:14 +0530

    numpy vs for loop speed test

--- numpy_vs_loops_speed_test.py
+++ numpy_vs_loops_speed_test.py
@@ -6,8 +6,12 @@ name_list = ["abhi", "ravi", "abhishek", "deepu","deepak","gopal","lokendra","yo
 city_list =["jaipur","delhi","mumbai","goa","ajmer","sikar"]
 Category_list = ["it","business","sports","teacher","full stack developer"]
 
-start =time.time()
+numpy_start =time.time()
 n = 1000000
+
+# ==========================================
+# 🚀 METHOD 1: NumPy (Fast Way)
+# ==========================================
 
 data = {
     'Customer-id': range(1, n+1),
@@ -19,11 +23,47 @@ data = {
     'Rating': np.round(np.random.uniform(1,5, size=n),1)
 }
 
-df = pd.DataFrame(data)
-end= time.time()
-df.to_csv("fast.csv", index=False)
+df_numpy = pd.DataFrame(data)
+numpy_end= time.time()
+time_taken = numpy_end-numpy_start
+print(f"NUMPY HOW MUCH TIME TAKEN {time_taken:.5f} in a seconds")
 
-print("----------RESULT--------------")
-print(f"file 'fast.csv' ban gayi hai with {n} rows")
-print (f"how much time taken {end-start:.5} second main")
-print("-------------END---------------")
+# ==========================================
+# 🐢 METHOD 2: For Loop (Slow Way)
+# ==========================================
+
+start = time.time()
+
+simple ={
+    'CUSTOMER-ID' :range(1, n+1),
+    'NAME': [np.random.choice(name_list)
+             for _ in range(n)],
+    'AGE': [np.random.randint(18,90)
+            for _ in range(n)],
+    'CITY':[np.random.choice(city_list)
+            for _ in range(n)],
+    'CATEGORY': [np.random.choice(Category_list)
+                 for _ in range(n)],
+    'AMOUNT': [np.random.randint(100,1000)
+               for _ in range(n)],
+    'RATING':[np.round(np.random.uniform(1,5),1)
+                       for _ in range(n)]
+            
+}
+
+df_loop = pd.DataFrame(simple)
+end= time.time()
+simple_time_taken = end-start
+print(f"FOR LOOP HOW MUCH TIME TAKEN {simple_time_taken:.5f}")
+
+# ==========================================
+# 🏆 RESULT
+# ==========================================
+
+print("-------------final result--------------")
+print(f"how much time taken in {time_taken:.5f} numpy")
+print (f"how much time taken {simple_time_taken:.5f} for loop")
+
+difference = simple_time_taken/time_taken
+print(f"conclsion : NUMPY is {difference:.2f} times FASTER!")
+df_numpy.to_csv("fast.csv", index=False)
