6de9452efd03a190e17b656d29e37dd0c6a755d2
AbhIfine <abhi926987@gmail.com>
2026-01-16 11:08:57 +0530

    added list

--- numpy_vs_loops_speed_test.py
+++ numpy_vs_loops_speed_test.py
@@ -1,3 +1,7 @@
 import numpy as np 
 import pandas as pd
 
+name_list = ["abhi", "ravi", "abhishek", "deepu","deepak","gopal","lokendra","yogesh","himanshu","lucky"]
+city_list =["jaipur","delhi","mumbai","goa","ajmer","sikar"]
+Category_list = ["it","business","sports","teacher","full stack developer"]
+
